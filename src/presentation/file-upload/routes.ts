555b1eebb7d1e576087097c7efbd28973072b188
EstebanSa25 <estebansalas2502@gmail.com>
2024-01-09 20:46:41 -0600

    Fin seccion 22

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
+import { TypeMiddleware } from '../middlewares/type.middleware';
+
+export class FileUploadRoutes {
+    static get routes(): Router {
+        const router = Router();
+
+        // Definir las rutas
+        const fileUploadService = new FileUploadService();
+        const controller = new FileUploadController(fileUploadService);
+        router.use([
+            FileUploadMiddleware.containFiles,
+            TypeMiddleware.validTypes(['users', 'products', 'categories']),
+        ]);
+        router.post('/single/:type', controller.uploadFile);
+        router.post('/multiple/:type', controller.uploadMultipleFiles);
+
+        return router;
+    }
+}
